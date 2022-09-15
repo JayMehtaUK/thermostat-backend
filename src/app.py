2555c3a86f6dc35b6ed86b09d84bb374c52832dd
Jay <jay_mehta@hotmail.co.uk>
2022-09-15 20:55:47 +0100

    Initial commit with basic backend

---
+++ src/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from thermostat_controller import ThermostatController
+
+
+app = Flask(__name__)
+thermostat_controller = ThermostatController()
+
+
+@app.route('/target-temperature', methods=['POST'])
+def target_temperature():
+    data = request.form
+    print(data.get("target-temperature"))
+    thermostat_controller.set_target_temperature(int(data.get("target-temperature")))
+    return jsonify(success=True)
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=8080)
