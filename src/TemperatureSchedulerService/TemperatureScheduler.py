10ef99556c1b60b87db3f7d7dbbd6e2c76430770
Jay <jay_mehta@hotmail.co.uk>
2022-11-21 21:01:49 +0000

    fix broken schedule

--- src/TemperatureSchedulerService/TemperatureScheduler.py
+++ src/TemperatureSchedulerService/TemperatureScheduler.py
@@ -3,22 +3,23 @@ from apscheduler.triggers.cron import CronTrigger
 
 
 class TemperatureScheduler:
+
     def create_schedule(self, target_temperature_service):
         schedule_list = self.read_schedule("schedule.yaml")
         scheduler = BackgroundScheduler(daemon=True)
 
         for cron, target_temperature in schedule_list:
-            scheduler.add_job(lambda: target_temperature_service.set_target_temperature(target_temperature), CronTrigger.from_crontab(cron))
+            scheduler.add_job(target_temperature_service.set_target_temperature, CronTrigger.from_crontab(cron), args=[target_temperature])
 
         scheduler.start()
 
     def read_schedule(self, file):
         # TODO: Actually read this from a file
-        temp_schedule = [('0 9 * * MON,TUE,FRI', 20),
+        temp_schedule = [('30 8 * * MON,TUE,FRI', 20),
                          ('30 10 * * MON,TUE,FRI', 17),
 
-                         ('30 7 * * WED,THU', 20),
-                         ('30 8 * * WED,THU', 16),
+                         ('0 7 * * WED,THU', 20),
+                         ('0 8 * * WED,THU', 16),
 
                          ('30 9 * * SAT,SUN', 20),
                          ('30 10 * * SAT,SUN', 16),
